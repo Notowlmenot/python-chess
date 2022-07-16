bab6bccb78fae56f838f75f47b7af6083862cc0b
menot <40204213+menot255@users.noreply.github.com>
2022-07-16 12:37:31 +1100

    Add files via upload

---
+++ Chess.py
@@ -0,0 +1,85 @@
+from Pieces import *
+import os
+WHITE = "white"
+BLACK = "black"
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def translator(current_figure):
+    if len(current_figure) >= 2 and 'A' <= current_figure[0].upper() <= 'H' and '1' <= current_figure[1] <= '8':
+        x = ord(current_figure[0].upper()) - ord('A')
+        y = int(current_figure[1]) - 1
+    else:
+        cls()
+        print('Некорректная фигура')
+        x, y = -1, -1
+    return x, y
+
+
+class Game:
+    def __init__(self):
+        self.colorturn = 1
+        self.gameboard = {}
+        self.playersturn = 'white'
+        self.placeFigures()
+        self.right_Turn = False
+
+    def printBoard(self):
+        print("  A | B | C | D | E | F | G | H |")
+        for i in range(0, 8):
+            print("-" * 32)
+            print(i + 1, end="|")
+            for j in range(0, 8):
+                item = self.gameboard.get((i, j), " ")
+                print(str(item) + ' |', end=" ")
+            print()
+        print("-" * 32)
+
+    def placeFigures(self):
+        figures = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
+        for i in range(0, 8):
+            self.gameboard[(1, i)] = Pawn(WHITE, 1, i)
+            self.gameboard[(6, i)] = Pawn(BLACK, 6, i)
+            self.gameboard[(0, i)] = figures[i](WHITE, 0, i)
+            self.gameboard[(7, i)] = figures[i](BLACK, 7, i)
+        for i in range(2, 6):
+            for k in range(0, 8):
+                self.gameboard[(i, k)] = ' '
+
+    def chooseFigure(self):
+        current_figure = input('Выберите фигуру: ')
+        x, y = translator(current_figure)
+        if x != -1:
+            if self.gameboard[(y,x)] != ' ':
+                    print(f'Выбранная фигура - {self.gameboard[(y, x)]}')
+                    current_move = input('Выберите ход: ')
+                    x1, y1 = translator(current_move)
+                    if self.gameboard[(y, x)].name != ' ' and self.gameboard[(y, x)].color != self.playersturn:
+                        cls()
+                        print('Выбрана фигура неверного цветка. Текущий ход -', self.playersturn)
+                        return
+                    if self.gameboard[(y, x)].move(x1, y1, self.gameboard):
+                        self.colorturn = self.colorturn * -1
+                        cls()
+                    else:
+                        cls()
+                        print('Некорректный ход. ')
+
+    def main(self):
+        while True:
+            self.printBoard()
+            if self.colorturn == 1:
+                print('Ход белых.')
+                self.playersturn = 'white'
+            else:
+                print('Ход черных.')
+                self.playersturn = 'black'
+
+            self.chooseFigure()
+
+
+if __name__ == "__main__":
+    Game().main()
---
+++ Pieces.py
@@ -0,0 +1,218 @@
+WHITE = "white"
+BLACK = "black"
+
+
+class Piece:
+    def __init__(self, color, y, x):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.name = ''
+
+    def __str__(self):
+        return self.name
+
+
+class Pawn(Piece):
+    is_first = True
+
+    def __init__(self, color, x, y):
+        super().__init__(color, x, y)
+        if self.color == WHITE:
+            self.name = 'P'
+
+        else:
+            self.name = 'p'
+
+    def move(self, x, y, gameboard):
+        # Piece_color = self.color
+        if self.color == WHITE:
+            kf = -1
+
+        else:
+            kf = 1
+
+        if (self.is_first == True and (self.y - y) * kf == 2 and self.x - x == 0) or ((self.y - y) * kf == 1 and self.x - x == 0) or ((self.y - y) * kf == 1 and abs(self.x - x) == 1 and gameboard[(y, x)] != ' ' and gameboard[(y, x)].color != self.color):
+            if (abs(self.y - y) == 1) and (abs(self.x - x) != 1) and (gameboard[(y, x)] != ' ') and (
+                    gameboard[(y, x)].color != self.color):
+                return
+            else:
+                gameboard[(y, x)] = gameboard[(self.y, self.x)]
+                gameboard[(self.y, self.x)] = ' '
+                print(abs(self.y - y))
+                self.x, self.y = x, y
+                self.is_first = False
+                return True
+
+
+class Knight(Piece):
+    def __init__(self, color, x, y):
+        super().__init__(color, x, y)
+        if self.color == WHITE:
+            self.name = 'N'
+        else:
+            self.name = 'n'
+
+    def move(self, x, y, gameboard):
+        if (abs(x - self.x) == 2 and abs(y - self.y) == 1) or (abs(x - self.x) == 1 and abs(y - self.y) == 2):
+            if (gameboard[(y, x)] == ' ') or (gameboard[(y, x)].color != self.color):
+                gameboard[(y, x)] = gameboard[(self.y, self.x)]
+                gameboard[(self.y, self.x)] = ' '
+                self.x, self.y = x, y
+                return True
+            else:
+                return False
+
+
+class Rook(Piece):
+    def __init__(self, color, x, y):
+        super().__init__(color, x, y)
+        if self.color == WHITE:
+            self.name = 'R'
+        else:
+            self.name = 'r'
+
+    def move(self, x, y, gameboard):
+        is_Trueturn = False
+        is_break = False
+        if self.x - x == 0:
+            direction = (self.y - y) // abs(self.y - y)
+            for i in range(direction, (self.y - y), direction):
+                if gameboard[(self.y - i, self.x)] == ' ':
+                    is_Trueturn = True
+                else:
+                    is_Trueturn = False
+                    is_break = True
+                    break
+            if not is_break and (gameboard[(y, x)] == ' ' or gameboard[(y, x)].color != self.color):
+                is_Trueturn = True
+            else:
+                is_Trueturn = False
+
+        elif self.y == y:
+            direction = (self.x - x) // abs(self.x - x)
+            for i in range(direction, (self.x - x), direction):
+                if gameboard[(self.y, self.x - i)] == ' ':
+                    is_Trueturn = True
+                else:
+                    is_Trueturn = False
+                    is_break = True
+                    break
+            if not is_break and (gameboard[(y, x)] == ' ' or gameboard[(y, x)].color != self.color):
+                is_Trueturn = True
+            else:
+                is_Trueturn = False
+
+        if is_Trueturn == True:
+            gameboard[(y, x)] = gameboard[(self.y, self.x)]
+            gameboard[(self.y, self.x)] = ' '
+            self.x, self.y = x, y
+            return True
+
+
+class Bishop(Piece):
+    def __init__(self, color, x, y):
+        super().__init__(color, x, y)
+        if self.color == WHITE:
+            self.name = 'B'
+        else:
+            self.name = 'b'
+
+    def move(self, x, y, gameboard):
+        is_Trueturn = False
+        is_break = False
+        if abs(self.x - x) == abs(self.y - y):
+            x_dir, y_dir = (x - self.x) // abs(self.x - x), (y - self.y) // abs(self.y - y)
+            for i in range(1, abs(self.x - x)):
+                if gameboard[(self.x + i * x_dir, self.y + i * y_dir)] == ' ':
+                    is_Trueturn = True
+                else:
+                    is_Trueturn = False
+                    is_break = True
+                    break
+            if not is_break and (gameboard[(y, x)] == ' ' or gameboard[(y, x)].color != self.color):
+                is_Trueturn = True
+            else:
+                is_Trueturn = False
+            if is_Trueturn:
+                gameboard[(y, x)] = gameboard[(self.y, self.x)]
+                gameboard[(self.y, self.x)] = ' '
+                self.x, self.y = x, y
+                return True
+
+
+class Queen(Piece):
+    def __init__(self, color, x, y):
+        super().__init__(color, x, y)
+        if self.color == WHITE:
+            self.name = 'Q'
+        else:
+            self.name = 'q'
+
+    def move(self, x, y, gameboard):
+        is_break = False
+        is_Trueturn = True
+        if self.x - x == 0:
+            direction = (self.y - y) // abs(self.y - y)
+            for i in range(direction, (self.y - y), direction):
+                if gameboard[(self.y - i, self.x)] == ' ':
+                    is_Trueturn = True
+                else:
+                    is_Trueturn = False
+                    is_break = True
+                    break
+            if not is_break and (gameboard[(y, x)] == ' ' or gameboard[(y, x)].color != self.color):
+                is_Trueturn = True
+            else:
+                is_Trueturn = False
+
+        elif self.y - y == 0:
+            direction = (self.x - x) // abs(self.x - x)
+            for i in range(direction, (self.x - x), direction):
+                if gameboard[(self.y, self.x - i)] == ' ':
+                    is_Trueturn = True
+                else:
+                    is_Trueturn = False
+                    is_break = True
+                    break
+            if not is_break and (gameboard[(y, x)] == ' ' or gameboard[(y, x)].color != self.color):
+                is_Trueturn = True
+            else:
+                is_Trueturn = False
+
+        elif abs(self.x - x) == abs(self.y - y):
+            x_dir, y_dir = (x - self.x) // abs(self.x - x), (y - self.y) // abs(self.y - y)
+            for i in range(1, abs(self.x - x)):
+                if gameboard[(self.x + i * x_dir, self.y + i * y_dir)] == ' ':
+                    is_Trueturn = True
+                else:
+                    is_Trueturn = False
+                    is_break = True
+                    break
+            if not is_break and (gameboard[(y, x)] == ' ' or gameboard[(y, x)].color != self.color):
+                is_Trueturn = True
+            else:
+                is_Trueturn = False
+
+        if is_Trueturn:
+            gameboard[(y, x)] = gameboard[(self.y, self.x)]
+            gameboard[(self.y, self.x)] = ' '
+            self.x, self.y = x, y
+            return True
+
+
+class King(Piece):
+    def __init__(self, color, x, y):
+        super().__init__(color, x, y)
+        if self.color == WHITE:
+            self.name = 'K'
+        else:
+            self.name = 'k'
+
+    def move(self, x, y, gameboard):
+        if abs(self.x - x) < 2 and (abs(self.y - y) < 2) and (
+                gameboard[(y, x)] == ' ' or gameboard[(y, x)].color != self.color):
+            gameboard[(y, x)] = gameboard[(self.y, self.x)]
+            gameboard[(self.y, self.x)] = ' '
+            self.x, self.y = x, y
+            return True
